752abe90ab704563c51b4edf6d08f4674504b647
aemino <aeminodev@gmail.com>
2019-01-06 11:34:00 -0800

    Use DateTime rather than Date for timestamps

--- server/Sources/App/Models/Update.swift
+++ server/Sources/App/Models/Update.swift
@@ -2,7 +2,7 @@ import Vapor
 import Fluent
 
 struct Update<UpdateModel: AnyModel>: Content {
-    var timestamp: Date
+    var timestamp: DateTime
     var model: String
 
     var user: GoogleProfile?
